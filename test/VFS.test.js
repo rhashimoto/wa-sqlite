51d8bc123eece340714d36454e659e59f191e795
Roy Hashimoto <roy@shoestringresearch.com>
2021-04-24 14:51:23 -0700

    Update VFS.test.js.

--- test/VFS.test.js
+++ test/VFS.test.js
@@ -1,50 +1,55 @@
 // @ts-ignore
-import { SQLiteReady, Database } from './SQLite.js';
+import SQLiteModuleFactory from '../dist/wa-sqlite-async.mjs';
+import * as SQLite from '../src/sqlite-api.js';
 import { MemoryAsyncVFS } from './MemoryAsyncVFS.js';
 import { MemoryVFS } from './MemoryVFS.js';
 
 import GOOG from './GOOG.js';
 
-async function loadSampleTable(db) {
-  await db.sql`
+async function loadSampleTable(sql) {
+  await sql`
     PRAGMA journal_mode = MEMORY;
     DROP TABLE IF EXISTS goog;
     CREATE TABLE goog (${GOOG.columns.join(',')});
   `;
   for (const row of GOOG.rows) {
-    await db.sql`INSERT INTO goog VALUES (${row.join(',')})`;
+    await sql`INSERT INTO goog VALUES (${row.join(',')})`;
   }
 }
 
 describe('VFS', function() {
-  let vfs;
+  /** @type {SQLite.SQLiteAPI} */ let sqlite3;
   beforeAll(async function() {
-    const SQLite = await SQLiteReady;
+    const SQLiteModule = await SQLiteModuleFactory();
 
-    vfs = new MemoryAsyncVFS();
-    SQLite.registerVFS('mem', vfs);
+    const vfs = new MemoryVFS();
+    SQLiteModule.registerVFS('mem', vfs);
+
+    sqlite3 = SQLite.Factory(SQLiteModule);
   });
 
-  let db;
+  let db, sql;
   beforeEach(async function() {
-    db = await Database.open('foo', 'mem');
+    db = await sqlite3.open_v2('foo', 0x06, 'unix');
+    sql = SQLite.tag(sqlite3, db);
   });
 
   afterEach(async function() {
-    await db.close();
+    await sqlite3.close(db);
   });
 
   it('persists', async function() {
     // Load data into the database.
-    await loadSampleTable(db);
-    const resultA = await db.sql`SELECT COUNT(*) FROM goog`;
+    await loadSampleTable(sql);
+    const resultA = await sql`SELECT COUNT(*) FROM goog`;
     expect(resultA[0].rows[0][0]).toBeGreaterThan(0);
 
     // Close and reopen the database.
-    await db.close();
-    db = await Database.open('foo', 'mem');
+    await sqlite3.close(db);
+    db = await sqlite3.open_v2('foo', 0x06, 'unix');
+    sql = SQLite.tag(sqlite3, db);
 
-    const resultB = await db.sql`SELECT COUNT(*) FROM goog`;
+    const resultB = await sql`SELECT COUNT(*) FROM goog`;
     expect(resultB[0].rows[0][0]).toBe(resultA[0].rows[0][0]);
   });
 
@@ -54,19 +59,20 @@ describe('VFS', function() {
 
     const timestamp = Date.now();
     for (let i = 0; i < N; ++i) {
-      let db = await Database.open('foobar', VFS_NAME);
-      await loadSampleTable(db);
-      const resultA = await db.sql`SELECT SUM(Volume) FROM goog`;
+      let db = await sqlite3.open_v2('foobar', 0x06, VFS_NAME);
+      let sql = SQLite.tag(sqlite3, db);
+      await loadSampleTable(sql);
+      const resultA = await sql`SELECT SUM(Volume) FROM goog`;
       expect(resultA[0].rows[0][0]).toBeGreaterThan(0);
 
-      await db.close();
-      db = await Database.open('foobar', VFS_NAME);
-
-      const resultB = await db.sql`SELECT SUM(Volume) FROM goog`;
+      await sqlite3.close(db);
+      db = await sqlite3.open_v2('foobar', 0x06, VFS_NAME);
+      sql = SQLite.tag(sqlite3, db);
+  
+      const resultB = await sql`SELECT SUM(Volume) FROM goog`;
       expect(resultB[0].rows[0][0]).toBe(resultA[0].rows[0][0]);
 
-      await db.sql`VACUUM`;
-      await db.close();
+      await sqlite3.close(db);
     }
     console.log('elapsed', (Date.now() - timestamp) / N);
   });
