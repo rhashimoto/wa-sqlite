fa7ccec88d71d42bf705ba6ab74aa18b3277c249
Roy Hashimoto <roy@shoestringresearch.com>
2021-05-01 12:18:01 -0700

    Code polishing.

--- src/examples/tag.js
+++ src/examples/tag.js
@@ -11,38 +11,40 @@ import * as SQLite from '../sqlite-api.js';
  * `;
  * ```
  * The returned Promise value contains an array of results for each
- * SQL statement that produces output.
+ * SQL statement that produces output. Each result is an object with
+ * properties `columns` (array of names) and `rows` (array of array
+ * of values).
  * @param {SQLite.SQLiteAPI} sqlite3 
  * @param {number} db
- * @returns {function(TemplateStringsArray, ...any): Promise<object>}
+ * @returns {function(TemplateStringsArray, ...any): Promise<object[]>}
  */
  export function tag(sqlite3, db) {
   return async function(strings, ...values) {
-    let interleaved = [];
+    // Assemble the template string components.
+    const interleaved = [];
     strings.forEach((s, i) => {
       interleaved.push(s, values[i]);
     });
+    const sql = interleaved.join('');
 
     // Transfer the SQL to WASM memory. We set up a try-finally block
     // to ensure that the memory is always freed.
-    let results = [];
-    const str = sqlite3.str_new(db, interleaved.join(''));
+    const results = [];
+    const str = sqlite3.str_new(db, sql);
     try {
-      // Initialize the prepared statement state that will evolve
-      // as we progress through the SQL.
-      /** @type {*} */ let prepared = { sql: sqlite3.str_value(str) };
-      while (true) {
-        // Prepare the next statement. Another try-finally goes here
-        // to ensure that each prepared statement is finalized.
-        if (!(prepared = await sqlite3.prepare_v2(db, prepared.sql))) {
-          break;
-        }
+      // Traverse and prepare the SQL, statement by statement.
+      /** @type {object} */ let prepared = { sql: sqlite3.str_value(str) };
+      while ((prepared = await sqlite3.prepare_v2(db, prepared.sql))) {
+        // Another try-finally goes here to ensure that each prepared
+        // statement is finalized.
         try {
-          // Step through the rows.
+          // Step through the rows produced by the statement.
           const rows = [];
           const columns = sqlite3.column_names(prepared.stmt)
           while (await sqlite3.step(prepared.stmt) === SQLite.SQLITE_ROW) {
-            // Collect row elements.
+            // Collect row elements. sqlite3.row is an API convenience
+            // function (not in the C API) that extracts values for all
+            // the columns of the row.
             const row = sqlite3.row(prepared.stmt);
             rows.push(row);
           }
