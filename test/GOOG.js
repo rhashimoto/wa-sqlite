c9105fafe8a8b241cb41d7f543ae2f3bd2a123a3
Roy Hashimoto <roy@shoestringresearch.com>
2021-05-06 11:52:52 -0700

    Fix GOOG date formatting.

--- test/GOOG.js
+++ test/GOOG.js
@@ -1342,8 +1342,8 @@ const rows = raw.trim().split('\n').map((line, i) => {
       const d = new Date(Date.parse(field));
       const formatted =
         d.getFullYear() +
-        '-' + d.getMonth().toString().padStart(2, '0') +
-        '-' + d.getDay().toString().padStart(2, '0') +
+        '-' + (d.getMonth() + 1).toString().padStart(2, '0') +
+        '-' + d.getDate().toString().padStart(2, '0') +
         ' ' + d.getHours().toString().padStart(2, '0') +
         ':' + d.getMinutes().toString().padStart(2, '0');
       return formatted;
