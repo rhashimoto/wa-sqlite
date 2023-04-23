280700ff96eac2c1087e6896f500a8c94b586a37
Roy Hashimoto <roy@shoestringresearch.com>
2023-04-22 20:04:02 -0700

    Fix two-way demo path to monaco.

--- demo/index.js
+++ demo/index.js
@@ -20,7 +20,9 @@ import GOOG from '../test/GOOG.js';
 // This will need to be changed if using a package manager other than Yarn 2.
 // The value can also reference an external CDN, e.g.
 // https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.23.0/min/vs
-const MONACO_VS = '/.yarn/unplugged/monaco-editor-npm-0.34.1-03d887d213/node_modules/monaco-editor/dev/vs';
+const MONACO_VS = location.hostname.endsWith('localhost') ?
+  '/.yarn/unplugged/monaco-editor-npm-0.34.1-03d887d213/node_modules/monaco-editor/dev/vs' :
+  'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.34.1/min/vs';
 
 const DEFAULT_SQL = `
 -- Optionally select statements to execute.
