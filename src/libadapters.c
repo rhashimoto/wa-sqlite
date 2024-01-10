edda91d867ab4ee55457ba66fa7b581345016a57
Roy Hashimoto <roy@shoestringresearch.com>
2024-01-10 10:51:36 -0800

    Streamline adapters code.

--- src/libadapters.c
+++ src/libadapters.c
@@ -3,11 +3,20 @@
 #include <emscripten.h>
 #include <sqlite3.h>
 
-extern int ii(int i);
+extern int ii(void*, const char*, int i);
+extern int async_ii(void*, const char*, int i);
 
 int main() {
   sqlite3_initialize();
-  const int result = ii(42);
-  printf("result: %d\n", result);
+
+  {
+    const int result = ii((void*)42, "testSync", 10);
+    printf("result: %d\n", result);
+  }
+
+  {
+    const int result = async_ii((void*)42, "testAsync", 15);
+    printf("result: %d\n", result);
+  }
   return 0;
 }
