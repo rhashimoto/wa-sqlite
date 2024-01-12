2421a7539f54ecb013314c1b66e54f06b8109db5
Roy Hashimoto <roy@shoestringresearch.com>
2024-01-12 09:49:25 -0800

    Pass sqlite3_free address to JavaScript.

--- src/libadapters.c
+++ src/libadapters.c
@@ -263,6 +263,15 @@ int EMSCRIPTEN_KEEPALIVE adapter_vfs_register(
   return sqlite3_vfs_register(&vfs->base, makeDefault);
 }
 
+// Some SQLite API functions take a pointer to a function that frees
+// memory. Although we could add a C binding to a JavaScript function
+// that calls sqlite3_free(), it is more efficient to pass the sqlite3_free
+// function pointer directly. This function provides the C pointer to
+// JavaScript.
+void* EMSCRIPTEN_KEEPALIVE getSqliteFree() {
+  return sqlite3_free;
+}
+
 int main() {
   sqlite3_initialize();
   return 0;
